479bcce3be59b57cb317fe69a75dcd6454708fec
Prajwal H S <prajwalhs4india@gmail.com>
2025-05-06 11:16:30 +0530

    changes

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,3 @@
-// app/post/[slug]/page.tsx
-
 import { PortableText, type SanityDocument } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
@@ -7,63 +5,45 @@ import { client } from "@/sanity/client";
 import Link from "next/link";
 import Image from "next/image";
 
-const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]{
-  _id,
-  title,
-  slug,
-  publishedAt,
-  body,
-  mainImage {
-    asset->{
-      _id,
-      url
-    }
-  }
-}`;
+const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
 
-const builder = imageUrlBuilder(client);
-const urlFor = (source: SanityImageSource) => builder.image(source);
+const { projectId, dataset } = client.config();
+const urlFor = (source: SanityImageSource) =>
+  projectId && dataset
+    ? imageUrlBuilder({ projectId, dataset }).image(source)
+    : null;
 
 const options = { next: { revalidate: 30 } };
 
 export default async function PostPage({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }) {
-  const { slug } = params; // Get the slug value from params
-
   const post = await client.fetch<SanityDocument>(
     POST_QUERY,
-    { slug },
+    await params,
     options
   );
-
-  // Get the main image URL
-  const postImageUrl = post?.mainImage?.asset?.url || null;
+  const postImageUrl = post.image
+    ? urlFor(post.image)?.width(550).height(310).url()
+    : null;
 
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
-      <div>
-        {" "}
-        <Link href="/" className="hover:underline">
-          ← Back to posts
-        </Link>
-      </div>
-      {postImageUrl ? (
+      <Link href="/blog" className="hover:underline">
+        ← Back to posts
+      </Link>
+      {postImageUrl && (
         <Image
           src={postImageUrl}
-          alt={post.title || "Post image"}
-          width={550}
-          height={310}
-          className="rounded-xl object-cover aspect-video"
+          alt={post.title}
+          className="aspect-video rounded-xl"
+          width="550"
+          height="310"
         />
-      ) : (
-        <p className="text-red-500">Image not available</p>
       )}
-
       <h1 className="text-4xl font-bold mb-8">{post.title}</h1>
-
       <div className="prose">
         <p>Published: {new Date(post.publishedAt).toLocaleDateString()}</p>
         {Array.isArray(post.body) && <PortableText value={post.body} />}
